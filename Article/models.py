5ceef67ecf0805a470883bd82cd8bda8233501da
ksir <llk16@163.cmo>
2015-07-09 11:52:00 +0800

    migrations

--- Article/models.py
+++ Article/models.py
@@ -5,11 +5,11 @@ from django.db import models
 class Article(models.Model):
 	title       = models.CharField(max_length=255)
 	group       = models.CharField(max_length=32)
-    instime     = models.DateTimeField(auto_now=True)
-    uptime      = modles.DateTimeField(auto_now=True)
-    content     = modles.TextField()
-    tags        = modles.CharField(max_length=255)
-    author      = models.CharField(max_length=64)
+	instime     = models.DateTimeField(auto_now=True)
+	uptime      = models.DateTimeField(auto_now=True)
+	content     = models.TextField()
+	tags        = models.CharField(max_length=255)
+	author      = models.CharField(max_length=64)
 
-    def __unicode__(self):
-        return '%s by %s' %(self.name, self.author)
+	def __unicode__(self):
+		return '%s by %s'%(self.name, self.author)

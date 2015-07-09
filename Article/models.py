f001eec48e034912dccbd8fe3ea73950a1c4b145
ksir <llk16@163.com>
2015-07-09 11:03:55 +0800

    define Article model

--- Article/models.py
+++ Article/models.py
@@ -3,5 +3,13 @@ from django.db import models
 # Create your models here.
 
 class Article(models.Model):
-	title = models.CharField(max_length=255)
-	group = models.CharField(max_length=32)
+	title       = models.CharField(max_length=255)
+	group       = models.CharField(max_length=32)
+    instime     = models.DateTimeField(auto_now=True)
+    uptime      = modles.DateTimeField(auto_now=True)
+    content     = modles.TextField()
+    tags        = modles.CharField(max_length=255)
+    author      = models.CharField(max_length=64)
+
+    def __unicode__(self):
+        return '%s by %s' %(self.name, self.author)

88e4adaa04655663e59e7bfe897b92c75d9c0b76
ksir <llk16@163.cmo>
2015-07-21 17:48:45 +0800

    add article show

--- Article/urls.py
+++ Article/urls.py
@@ -28,4 +28,5 @@ urlpatterns = patterns('Article.views',
     url(r'^artmag/$', 'manager.adminArticleList', name='adminartlisturl'),
     url(r'^artmag/edit/$', 'manager.editArticle', name='editarturl'),
     url(r'^artmag/delete/(?P<artid>\d+)/$', 'manager.delArticle', name='deletearturl'),
+    url(r'^artmag/artshow/(?P<artid>\d+)/$', 'article.articlePreshow', name='artpreshowurl'),
 )
---
+++ Article/views/article.py
@@ -0,0 +1,26 @@
+#coding=utf-8
+
+from django.http import HttpResponse,HttpResponseRedirect
+from django.shortcuts import render_to_response,RequestContext
+from django.core.urlresolvers import reverse
+from website.common.CommonPaginator import SelfPaginator
+
+from Article.models import Article
+from Article.forms import editArticleForm
+import logging
+
+
+logger = logging.getLogger(__name__)
+
+def articlePreshow(request, artid):
+	'''预览文章'''
+	article = Article.objects.get(pk=int(artid))
+	if article:
+		kwvars = {
+			'article': article,
+			'request': request,
+		}
+        return render_to_response('Article/article.show.html', kwvars, RequestContext(request))
+
+    return HttpResponseRedirect(reverse('adminartlisturl'))
+
